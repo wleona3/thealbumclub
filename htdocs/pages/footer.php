9564608f29bb32f9ba11147430fce291ad12891c
Will Leonardi <will@reflexionsdata.com>
2016-02-17 19:57:02 -0500

    Better mobile layout

--- htdocs/pages/footer.php
+++ htdocs/pages/footer.php
@@ -3,6 +3,6 @@
 <div id="footer">
 	Copyright &copy;<?=date('Y')?> The Album Club. All rights reserved.
 	"Bornean Orangutan" copyright
-	&copy; <a href="https://www.flickr.com/photos/63971538@N07/">Dodik Putro.</a>
+	&copy;&nbsp;<a href="https://www.flickr.com/photos/63971538@N07/">Dodik Putro.</a>
 </div>
 </body>
