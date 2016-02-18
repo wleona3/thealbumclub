55fc18a3cc878ad90a71b2219b12d12343acf2b0
root <root@ip-172-31-10-127.ec2.internal>
2016-02-17 20:19:41 -0500

    More mobile tweaks

--- htdocs/pages/footer.php
+++ htdocs/pages/footer.php
@@ -4,7 +4,7 @@
 	<div id="mobile_social_links">
 		<a href="https://thealbumclub.bandcamp.com" target="_blank">bandcamp</a> - <a href="https://www.facebook.com/TheAlbumClubBand" target="_blank">facebook</a>
 	</div>
-	<div id="copyright">
+	<div id="legal">
 		Copyright &copy;<?=date('Y')?> The Album Club. All rights reserved.
 		"Bornean Orangutan" copyright
 		&copy;&nbsp;<a href="https://www.flickr.com/photos/63971538@N07/">Dodik Putro.</a>
