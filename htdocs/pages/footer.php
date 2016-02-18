d6960a99fc9031c4d427a37369bc4d4528209f2a
Will Leonardi <will@reflexionsdata.com>
2016-02-17 20:07:34 -0500

    More mobile layout cleanup

--- htdocs/pages/footer.php
+++ htdocs/pages/footer.php
@@ -1,8 +1,13 @@
 	</div> <!-- #body -->	
 </div> <!-- #wrapper -->
 <div id="footer">
-	Copyright &copy;<?=date('Y')?> The Album Club. All rights reserved.
-	"Bornean Orangutan" copyright
-	&copy;&nbsp;<a href="https://www.flickr.com/photos/63971538@N07/">Dodik Putro.</a>
+	<div id="mobile_social_links">
+		<a href="https://thealbumclub.bandcamp.com" target="_blank">bandcamp</a> - <a href="https://www.facebook.com/TheAlbumClubBand" target="_blank">facebook</a>
+	</div>
+	<div id="copyright">
+		Copyright &copy;<?=date('Y')?> The Album Club. All rights reserved.
+		"Bornean Orangutan" copyright
+		&copy;&nbsp;<a href="https://www.flickr.com/photos/63971538@N07/">Dodik Putro.</a>
+	</div>
 </div>
 </body>
